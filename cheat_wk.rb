72e225a45d01b4dcbdab802b2aa1e822c25e4201
wendars <wendy@contently.com>
2013-10-03 16:33:54 -0400

    Homework for 10/3, V2

--- cheat_wk.rb
+++ cheat_wk.rb
@@ -2,13 +2,24 @@
 
 # these are variables
 a = "Yo"
-b = "Bitch"
+b = "homie"
 
 # a condition
 if (a == b)
 	puts "#{a} dawg"
 else
 	puts "#{a} #{b}"
+end
+
+# another condition
+x = 3
+puts "You are walking in the forest, and you run into #{x} bears."
+if x == 1
+	puts "One bear. Easy as pie. Bear pie."
+elsif x == 2 
+	puts "Hmm... you might be okay."
+else
+	puts "Sorry. You are not Ron Swanson. Run!"
 end
 
 # for loops
@@ -29,6 +40,12 @@ d.each do |item|
 	puts item
 end
 
+# another each loop
+g = ["Walter", "Jesse", "Hank"]
+g.each do |g|
+	puts "Goodbye #{g}"
+end
+
 # begin, rescue, end
 begin
 	puts "Never get high " + 4
@@ -38,10 +55,11 @@ end
 
 # method 1
 def yoPerson(name)
-	"Yo " + name
+	puts "Yo " + name
 end
 
 # method 2
 def addFive(f)
-	f + 5
+	puts f + "5"
 end
+
